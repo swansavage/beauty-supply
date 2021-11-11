516412cb87362a15bb29dce634660eedfb0928bc
swansavage@github.com <swansavage@gmail.com>
2021-11-10 19:48:50 -0600

    added gatsby cloud plugin to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
